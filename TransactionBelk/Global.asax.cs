1f74260df1a9659c86268d6f35ca674f008eaa33
belkinbelkin <ellerin5770@gmail.com>
2016-02-24 13:52:16 +0400

    Update Global.asax.cs

--- TransactionBelk/Global.asax.cs
+++ TransactionBelk/Global.asax.cs
@@ -21,6 +21,7 @@ namespace TransactionBelk
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<TransactionContext>(null);
         }
     }
 }
