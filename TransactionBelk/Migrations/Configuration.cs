7b6306055051b4cf8353aa62d2399e09c5c1cde7
belkinbelkin <ellerin5770@gmail.com>
2016-02-24 13:14:46 +0400

    Update Configuration.cs

--- TransactionBelk/Migrations/Configuration.cs
+++ TransactionBelk/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace TransactionBelk.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(TransactionBelk.Models.TransactionContext context)
