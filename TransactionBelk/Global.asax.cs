33d29c9fab232d9a5d6b8e10a455036d6d3cd6f8
belkinbelkin <ellerin5770@gmail.com>
2016-02-24 13:27:52 +0400

    Update Global.asax.cs

--- TransactionBelk/Global.asax.cs
+++ TransactionBelk/Global.asax.cs
@@ -15,6 +15,7 @@ namespace TransactionBelk
         protected void Application_Start()
         {
             //Database.SetInitializer(new StatusInit());
+            Database.SetInitializer<TransactionContext>(null);
 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
